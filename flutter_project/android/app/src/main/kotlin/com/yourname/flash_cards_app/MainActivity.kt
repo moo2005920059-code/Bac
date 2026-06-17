a3db7bdd9dea4c6160575867ac304a8864ff13f1
Mohamed Al Debsawe <moo2005920059@gmail.com>
2026-06-17 15:04:50 +0300

    Delete flutter_project/android directory

--- flutter_project/android/app/src/main/kotlin/com/yourname/flash_cards_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yourname.flash_cards_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_project/android/app/src/main/kotlin/com/yourname/flashcardsapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yourname.flashcardsapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
